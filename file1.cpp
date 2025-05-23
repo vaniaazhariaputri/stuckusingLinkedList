ee36b0060e8e5c238590c4e6f13109fe19f43199
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-23 09:00:46 +0700

    Menampilkan invalid choice, dan memulai kembali

--- file1.cpp
+++ file1.cpp
@@ -121,4 +121,8 @@ int main()
             break;
         case 4:
         cout << "Exiting program." << endl;
-            
+        break;
+        default:
+            cout <<< "Invalid choice. Try again." << endl;
+            break;
+         
