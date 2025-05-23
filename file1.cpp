6a6987ca606812c430b81df5cd8dc333ee07a4c9
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-05-23 07:59:31 +0700

    Memasukkan class node, allocate memory, assign value,set pointer dan update pointer

--- file1.cpp
+++ file1.cpp
@@ -29,5 +29,12 @@ public:
     // Push operation: Insert an element onto the top of the stack
     int push(int value)
     {
+        Node *newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. assign value
+        newNode->next = top; // 3. set the next pointer of the new node to the top
+        top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
+    }
 
 }
